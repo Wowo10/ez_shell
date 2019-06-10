60f685de815b0432782291e1f80316cb0ef51e38
Wowo10 <wojtekwojtek529@gmail.com>
2019-06-10 20:30:53 +0200

    Added basic input capture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+fn wait_for_input() -> String {
+    use std::io::{stdin, stdout, Write};
+    let mut s = String::new();
+    let _ = stdout().flush();
+    stdin()
+        .read_line(&mut s)
+        .expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    s
+}
+
+fn prompt() {
+    print!("{}", "~> ");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let welcome_message =
+        "\nWelcome to EZ_Shell, input your command, you`re welcome to exit any time.\n\n";
+    println!("{}", welcome_message);
+    prompt();
+    let input = wait_for_input();
+    println!("{}", input);
 }
